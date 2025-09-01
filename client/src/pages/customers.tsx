8f3edbe477957938671e839d5755468aec3da0ee
victor <vabimbola@zojatech.com>
2025-09-01 01:03:24 +0100

    fixes

--- client/src/pages/customers.tsx
+++ client/src/pages/customers.tsx
@@ -12,7 +12,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Badge } from "@/components/ui/badge";
-import { Switch } from 'antd'
+import { Switch, Pagination } from 'antd'
 import { base_url } from "../config";
 import {
   Select,
