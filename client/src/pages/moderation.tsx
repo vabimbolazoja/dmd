ba81c8487f1b6100e8e01ffefcd29f94c4579050
victor <vabimbola@zojatech.com>
2025-09-10 04:41:39 +0100

    fixes

--- client/src/pages/moderation.tsx
+++ client/src/pages/moderation.tsx
@@ -250,7 +250,7 @@ export default function Products() {
                         <DialogTrigger asChild>
                             <Button className="bg-blue-600 hover:bg-blue-700">
                                 <Plus className="mr-2 h-4 w-4" />
-                                Add Config
+                                Add Delivery Setup
                             </Button>
                         </DialogTrigger>
                         <DialogContent customWidth="900px" className="max-h-[90vh] overflow-y-auto">
